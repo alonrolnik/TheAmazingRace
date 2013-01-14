c3d243375e0dcb6cecc98005d1fd10f4ce855b91
Alon Rolnik <alonrolnik@gmail.com>
2013-01-14 14:43:05 +0200

    add User class, ServerInterface class and Http_Interface class

---
+++ src/cse/bgu/finalandroidproject/theamazigrace/Server_Interface.java
@@ -0,0 +1,14 @@
+package cse.bgu.finalandroidproject.theamazigrace;
+
+public class Server_Interface extends Http_Interface{
+	
+	/**
+	 * send to the server registration request
+	 * @param user
+	 * @return true if user successfully register else return false
+	 */
+	public boolean register(User user){
+	
+		return false;// Not implemented
+	}
+}
---
+++ src/cse/bgu/finalandroidproject/theamazigrace/User.java
@@ -0,0 +1,14 @@
+package cse.bgu.finalandroidproject.theamazigrace;
+
+/**
+ * This class define user properties
+ * @author alon
+ *
+ */
+public class User {
+	private String name;
+	private String email;
+	private String password;
+	
+
+}
