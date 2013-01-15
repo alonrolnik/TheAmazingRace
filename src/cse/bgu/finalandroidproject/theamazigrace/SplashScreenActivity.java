83b6b9a9bf45688b24fddd5270a0a1d063be2535
Alon Rolnik <alonrolnik@gmail.com>
2013-01-15 15:47:13 +0200

    implement splash screen

--- src/cse/bgu/finalandroidproject/theamazigrace/SplashScreenActivity.java
+++ src/cse/bgu/finalandroidproject/theamazigrace/SplashScreenActivity.java
@@ -1,16 +1,33 @@
 package cse.bgu.finalandroidproject.theamazigrace;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 
 public class SplashScreenActivity extends Activity {
 
+    //how long in [msec] until we go to the next activity
+	private final int SPLASH_DISPLAY_LENGHT = 3000;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
-	}
+		
+        /* New Handler to start the Menu-Activity 
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the LogIn Activity. */
+                Intent intent = new Intent(SplashScreenActivity.this,LoginScreenActivity.class);
+                SplashScreenActivity.this.startActivity(intent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
