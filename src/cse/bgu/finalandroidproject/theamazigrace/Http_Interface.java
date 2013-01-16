f86fd3de2eaf0fcc79b5f6bca650423ca3e805fe
Alon Rolnik <alonrolnik@gmail.com>
2013-01-16 11:24:31 +0200

    start implemnt Http_interface

--- src/cse/bgu/finalandroidproject/theamazigrace/Http_Interface.java
+++ src/cse/bgu/finalandroidproject/theamazigrace/Http_Interface.java
@@ -1,15 +1,57 @@
 package cse.bgu.finalandroidproject.theamazigrace;
 
+import java.net.HttpURLConnection;
 import java.net.URI;
 
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 
-public class Http_Interface {
+
+public class Http_Interface{
 
 	//create functions for http get and post
 
 	private static final URI url = null;
-	HttpURLConnection
-
+// check for available connectivity
 	
+    // When user clicks button, calls AsyncTask.
+    // Before attempting to fetch the URL, makes sure that there is a network connection.
+        // Gets the URL from the UI's text field.
+        String stringUrl = urlText.getText().toString();
+        ConnectivityManager connMgr = (ConnectivityManager) 
+            getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+	if (networkInfo != null && networkInfo.isConnected()) {
+		// fetch data
+	} else {
+		// display error
+	}
+	
+    // Uses AsyncTask to create a task away from the main UI thread. This task takes a 
+    // URL string and uses it to create an HttpUrlConnection. Once the connection
+    // has been established, the AsyncTask downloads the contents of the webpage as
+    // an InputStream. Finally, the InputStream is converted into a string, which is
+    // displayed in the UI by the AsyncTask's onPostExecute method.
+    private class DownloadWebpageText extends AsyncTask {
+       @Override
+       protected String doInBackground(String... urls) {
+             
+           // params comes from the execute() call: params[0] is the url.
+           try {
+               return downloadUrl(urls[0]);
+           } catch (IOException e) {
+               return "Unable to retrieve web page. URL may be invalid.";
+           }
+       }
+       // onPostExecute displays the results of the AsyncTask.
+       @Override
+       protected void onPostExecute(String result) {
+           textView.setText(result);
+      }
+   }
+
 }
 
