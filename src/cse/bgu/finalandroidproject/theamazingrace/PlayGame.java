c553d824e565ab6c82b5ba831154f47f3866ece0
Alon Rolnik <alonrolnik@gmail.com>
2013-02-20 17:56:19 +0200

    inserting Map object with location provider

--- src/cse/bgu/finalandroidproject/theamazingrace/PlayGame.java
+++ src/cse/bgu/finalandroidproject/theamazingrace/PlayGame.java
@@ -1,17 +1,189 @@
 package cse.bgu.finalandroidproject.theamazingrace;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-public class PlayGame extends Activity {
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.support.v4.app.DialogFragment;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.android.location.R;
+import com.example.android.location.LocationActivity.EnableGpsDialogFragment;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class PlayGame extends android.support.v4.app.FragmentActivity 
+		implements	 OnMapClickListener, OnMapLongClickListener, OnCameraChangeListener{
+
+    /**
+     * Note that this may be null if the Google Play services APK is not available.
+     */
+    private GoogleMap mMap;
+    private TextView mTapTextView;
+    private TextView mCameraTextView;
+
+    private TextView mLatLng;
+    private TextView mAddress;
+    private Button mFineProviderButton;
+    private Button mBothProviderButton;
+    private LocationManager mLocationManager;
+    private Handler mHandler;
+    private boolean mGeocoderAvailable;
+    private boolean mUseFine;
+    private boolean mUseBoth;
+
+    // Keys for maintaining UI states after rotation.
+    private static final String KEY_FINE = "use_fine";
+    private static final String KEY_BOTH = "use_both";
+    // UI handler codes.
+    private static final int UPDATE_ADDRESS = 1;
+    private static final int UPDATE_LATLNG = 2;
+
+    private static final int TEN_SECONDS = 10000;
+    private static final int TEN_METERS = 10;
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_play_game);
+        mTapTextView = (TextView) findViewById(R.id.tap_text);
+        mCameraTextView = (TextView) findViewById(R.id.camera_text);
 
+		setUpMapIfNeeded();
 	}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if the GPS setting is currently enabled on the device.
+        // This verification should be done during onStart() because the system calls this method
+        // when the user returns to the activity, which ensures the desired location provider is
+        // enabled each time the activity resumes from the stopped state.
+        LocationManager locationManager =
+                (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!gpsEnabled) {
+            // Build an alert dialog here that requests that the user enable
+            // the location services, then when the user clicks the "OK" button,
+            // call enableLocationSettings()
+            new EnableGpsDialogFragment().show(getSupportFragmentManager(), "enableGpsDialog");
+        }
+    }
+
+    
+    // Method to launch Settings
+    private void enableLocationSettings() {
+        Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(settingsIntent);
+    }
+
+    // Stop receiving location updates whenever the Activity becomes invisible.
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mLocationManager.removeUpdates(listener);
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView
+     * MapView}) will show a prompt for the user to install/update the Google Play services APK on
+     * their device.
+     * <p>
+     * A user can return to this Activity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the Activity may not have been
+     * completely destroyed during this process (it is likely that it would only be stopped or
+     * paused), {@link #onCreate(Bundle)} may not be called again so we should call this method in
+     * {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnCameraChangeListener(this);
+
+    }
+    
+    /**
+     * When the player want to know if he arrived to the right checkpoint he should press 
+     * the "check my location !!!" button.
+     * This should check if he arrived to the right place
+     */
+    public void checkMyLocation(View view){
+    	
+    }
+    
+
+    @Override
+    public void onMapClick(LatLng point) {
+        mTapTextView.setText("tapped, point=" + point);
+    }
+
+    @Override
+    public void onMapLongClick(LatLng point) {
+        mTapTextView.setText("long pressed, point=" + point);
+    }
+
+    @Override
+    public void onCameraChange(final CameraPosition position) {
+        mCameraTextView.setText(position.toString());
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +191,64 @@ public class PlayGame extends Activity {
 		getMenuInflater().inflate(R.menu.activity_play_game, menu);
 		return true;
 	}
+	
+	
+    // AsyncTask encapsulating the reverse-geocoding API.  Since the geocoder API is blocked,
+    // we do not want to invoke it from the UI thread.
+    private class ReverseGeocodingTask extends AsyncTask<Location, Void, Void> {
+        Context mContext;
+
+        public ReverseGeocodingTask(Context context) {
+            super();
+            mContext = context;
+        }
+
+        @Override
+        protected Void doInBackground(Location... params) {
+            Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
+
+            Location loc = params[0];
+            List<Address> addresses = null;
+            try {
+                addresses = geocoder.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+                // Update address field with the exception.
+                Message.obtain(mHandler, UPDATE_ADDRESS, e.toString()).sendToTarget();
+            }
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+                // Format the first line of address (if available), city, and country name.
+                String addressText = String.format("%s, %s, %s",
+                        address.getMaxAddressLineIndex() > 0 ? address.getAddressLine(0) : "",
+                        address.getLocality(),
+                        address.getCountryName());
+                // Update address field on UI.
+                Message.obtain(mHandler, UPDATE_ADDRESS, addressText).sendToTarget();
+            }
+            return null;
+        }
+    }
+
+
+    /**
+     * Dialog to prompt users to enable GPS on the device.
+     */
+    private class EnableGpsDialogFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.enable_gps)
+                    .setMessage(R.string.enable_gps_dialog)
+                    .setPositiveButton(R.string.enable_gps, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            enableLocationSettings();
+                        }
+                    })
+                    .create();
+        }
+    }
 
 }
